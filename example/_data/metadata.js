f91e6d641f846694630fb6849ece28d96de68bca
Khaleel Gibran <khalby786@gmail.com>
2024-03-31 21:47:30 +0400

    (docs) example website

---
+++ example/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "slack-cms example",
+	url: "https://khalby786.github.io/slack-cms/",
+	language: "en",
+	description: "A simple example of a website built using slack-cms.",
+	author: {
+		name: "Khaleel Gibran",
+		email: "khalby786@gmail.com",
+		url: "https://khaleelgibran.com/"
+	}
+}
---
+++ example/_data/posts.js
@@ -0,0 +1,38 @@
+const { SlackCMS } = require("../../lib/src/index.js");
+
+const cms = new SlackCMS(process.env.SLACK_TOKEN, {
+	allowEmptyMetadata: true,
+	slackParserOptions: {
+		escapeHTML: false,
+		slackOnly: true,
+		noExtraSpanTags: true,
+		slackCallbacks: {
+			channel: (channel) => {
+				return `<a href="https://hackclub.slack.com/archives/${channel.id}" class="slack-channel" data-channel-id="${channel.id}">#${channel.name}</a>`;
+			},
+			user: (user) => {
+				return `<a href="https://hackclub.slack.com/team/${user.id}" class="slack-user" data-user-id="${user.id}">@${
+					user.name ? user.name : user.id
+				}</a>`;
+			},
+		},
+	},
+});
+
+module.exports = async function () {
+	let data = await cms.getPosts("C05B6DBN802");
+	let posts = [];
+
+	// if post.attachments.from_url is present, it means the attachment is a post from another channel and we should add it to the post content
+	data.forEach(async (post) => {
+		if (post.attachments && post.attachments[0].from_url) {
+			post.extraContent = `${post.attachments[0].fallback}`;
+			posts.push(post);
+		} else {
+			posts.push(post);
+		}
+	});
+
+	console.dir(posts[0], { depth: null });
+	return posts;
+};
